91746454f93b21afc6edd5e761e4ba96d527a1c4
Alexander Baraley <alexander.baraley@gmail.com>
2019-08-10 22:03:08 +0300

    add answer reading ability

---
+++ EightBall/Services/TextPronouncer.swift
@@ -0,0 +1,35 @@
+//
+//  TextPronouncer.swift
+//  EightBall
+//
+//  Created by Alexander Baraley on 8/10/19.
+//  Copyright © 2019 Alexander Baraley. All rights reserved.
+//
+
+import AVFoundation
+
+class TextPronouncer {
+	
+	private var speechSynthesizer: AVSpeechSynthesizer = {
+		let audioSession = AVAudioSession.sharedInstance()
+		try? audioSession.setCategory(AVAudioSession.Category.ambient, options: [.duckOthers])
+		return AVSpeechSynthesizer()
+	}()
+	
+	func pronounce(_ text: String) {
+		guard !text.isEmpty else { return }
+		let utterance = AVSpeechUtterance(string: text)
+		let voice = AVSpeechSynthesisVoice.speechVoices().first {
+			$0.identifier == "com.apple.ttsbundle.Samantha-premium"
+		}
+		utterance.voice = voice ?? AVSpeechSynthesisVoice(language: "en-US")
+		stopPronouncing()
+		speechSynthesizer.speak(utterance)
+	}
+	
+	func stopPronouncing() {
+		if speechSynthesizer.isSpeaking == true {
+			speechSynthesizer.stopSpeaking(at: .immediate)
+		}
+	}
+}
